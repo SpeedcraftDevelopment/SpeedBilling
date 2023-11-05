b3f7cd148f1162d3e03b201f5eeb488fc95d921d
EldzejGM <78844172+eldzejgm@users.noreply.github.com>
2023-11-05 13:50:18 +0100

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+$host = 'localhost';
+$username = 'user';
+$password = 'password';
+$database = 'database';
+
+$db = new mysqli($host, $username, $password, $database);
+
+$theme = 'beta';
+?>
