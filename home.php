bd7c94052d35dad0942b0dda21e85a78df77e03d
EldzejGM <78844172+eldzejgm@users.noreply.github.com>
2023-11-05 13:36:30 +0100

    Create home.php

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+
+include 'config/config.php';
+
+$user_id = $_SESSION['user_id'];
+
+$query = "SELECT username FROM users WHERE id = ?";
+$stmt = $db->prepare($query);
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$stmt->store_result();
+
+if ($stmt->num_rows == 1) {
+    $stmt->bind_result($usrname);
+    $stmt->fetch();
+} else {
+    // Obsłuż sytuację, w której nie znaleziono użytkownika o podanym ID
+    echo "Nie znaleziono użytkownika o podanym ID.";
+    exit;
+}
+include "themes/$theme/home.php";
+?>
